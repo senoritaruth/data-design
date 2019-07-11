181f1de5abc926de3f8176fe422af44c3fe1ef4c
Ruth Cisneros <senoritaruth@gmail.com>
2019-07-11 01:30:17 -0600

    added table of likes, dislikes, traits, and tech. added more details to her persona.

--- epic/use-case.php
+++ epic/use-case.php
@@ -5,12 +5,34 @@
 		<title>User Story, Use Case, Interaction Flow</title>
 	</head>
 	<body>
-		<h1>User Story</h1>
-		Molly is attending a gala event and needs to find an evening cape
-		<br>
-		<H1>Use Case</H1>
-		Molly has logged into her favorite high-end online store, <a href="https://www.bergdorfgoodman.com">Bergdorf Goodman</a>, and is searching for a black cape by designer and color.
-	<br>
+		<div id="userstory">
+		<h2>User Story</h2>
+		Molly is attending a gala event and needs to find an evening cape.
+		</div>
+		<div id="usecase">
+			<h2>Use Case</h2>
+			Molly has logged into her favorite high-end online store, <a href="https://www.bergdorfgoodman.com">Bergdorf Goodman</a>, and is searching for a black cape by designer and color.
+		</div>
+		<div id="prepostcond">
+			Preconditions:
+			<ul>
+				<li>Molly is a registered user of <a href="https://www.bergdorfgoodman.com">Bergdorf Goodman</a>.</li>
+			</ul>
+			Postcondition:
+			<ul>
+				<li>Molly finds the cape she would like to purchase.</li>
+		</div>
+		<div id="interaction">
+		<h2>Interaction Flow</h2>
+		<ul>
+			<li>Molly logs into BG.com</li>
+			<li>She clicks on Women's Clothing section,</li>
+			<li>She clicks on the Type dropdown to filter by Capes.</li>
+			<li>She then clicks on the Color dropdown to filter to black.</li>
+			<li>She sorts by highest to lowest price.</li>
+			<li>She selects the prettiest cape of the top six results.</li>
+		</ul>
+		</div>
 		<a href="index.php">Data Design Home</a>
 	</body>
 </html>
